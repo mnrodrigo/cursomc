c00196a8b8801ed53a6a679a6f8ded56a9beee6f
Manoel Rodrigo do Nascimento <rodrigo@prosig.com.br>
2018-04-30 17:23:57 -0300

    Primeiro codigo de teste de Rest

---
+++ src/main/java/br/com/prosig/cursoc/resources/CategoriaResource.java
@@ -0,0 +1,16 @@
+package br.com.prosig.cursoc.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/Categorias")
+public class CategoriaResource {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar() {
+		return "Rest esta funcionando";
+	}
+
+}
